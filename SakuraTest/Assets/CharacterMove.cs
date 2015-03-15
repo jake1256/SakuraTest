efe9194e5dd5fc4a38113f482d486eb0d895c57a
Jake <rock.zeke@gmail.com>
2015-03-15 17:08:57 +0900

    Initial Commit

---
+++ SakuraTest/Assets/CharacterMove.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMove : MonoBehaviour {
+
+	float forwardSpeed = 3.0f;
+	float backwardSpeed = 1.0f;
+	float rotateSpeed = 10.0f;
+	Animator anim;
+	CharacterController characterController;
+	void Start () {
+		anim = GetComponent<Animator>();
+		characterController = GetComponent<CharacterController>();
+	}
+	
+	void Update () {
+		float v = Input.GetAxis("Vertical");
+		float h = Input.GetAxis("Horizontal");
+
+		Vector3 velocity = new Vector3(0, 0, v);
+		velocity = transform.TransformDirection(velocity);
+		if (v > 0) {
+			velocity *= forwardSpeed;
+		} else if (v < 0) {
+			velocity *= backwardSpeed;
+		}
+
+		anim.SetFloat("Speed" , v);
+
+		characterController.Move(velocity * Time.deltaTime);
+		transform.Rotate(0, h * rotateSpeed, 0);
+
+		if(Input.GetButtonDown("Jump")){
+			anim.SetBool("IsSalute" , true);
+		}else{
+			anim.SetBool("IsSalute" , false);
+		}
+	}
+}
---
+++ SakuraTest/Assets/SakuraCreater.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SakuraCreater : MonoBehaviour {
+	
+	public GameObject flower;
+	public ParticleSystem sakuraSakuEffect;
+	public ParticleSystem sakuraTiruEffect;
+	// Use this for initialization
+	void Start () {
+		sakuraSakuEffect.Stop();
+		sakuraTiruEffect.Stop();
+	}
+	
+	/// <summary>
+	/// BoxColliderのIsTriggerをONにしている際に、
+	/// オブジェクトが範囲に入った際に呼ばれるイベント
+	/// </summary>
+	/// <param name="col">Col.</param>
+	void OnTriggerEnter(Collider col){
+		
+		for(int i = 0 ; i < 5 ; i++){
+			createSakura();
+		}
+		
+		sakuraSakuEffect.Play();
+		sakuraTiruEffect.Play();
+	}
+	
+	/// <summary>
+	/// 桜の花を一つ作成します。
+	/// </summary>
+	/// <returns>The sakura.</returns>
+	private void createSakura(){
+		Vector3 pos = this.gameObject.transform.position;
+		
+		float x = Random.Range(-1.0f , 1.0f);
+		float y = Random.Range(1.0f , 2.0f);
+		float z = Random.Range(-1.0f , 1.0f);
+		
+		pos.x += x;
+		pos.y += y;
+		pos.z += z;
+		
+		GameObject obj = Instantiate(flower , pos , Quaternion.identity) as GameObject;
+		obj.transform.SetParent(this.gameObject.transform);
+	}
+}
